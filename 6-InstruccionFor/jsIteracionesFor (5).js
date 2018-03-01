8a010485317c7070d4d127a80e37ed2623e5f438
alumno <alumno>
2018-03-01 10:14:55 -0300

    hol

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,5 +1,11 @@
 function Mostrar()
 {
+    var numero=i;
+    numero=1;
+    numero=prompt("ingrese numero");
+        for(i=1;numero!=9;numero++){
+            parseInt(numero);
+        }
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,5 +1,16 @@
 function Mostrar()
-{
+{var numero=i=1
+numero=prompt("ingresar numero");
+var contador=0;
+
+
+    for(let i=1;numero!=9;numero++){
+        if(i%2==0){
+            contador++
+            console.log(i)
+        }}                                    //numero pares
+
+
 
 
 
