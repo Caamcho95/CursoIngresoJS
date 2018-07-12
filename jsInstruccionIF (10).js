143c736d33eb33a0a1b0e795ab8bc3eab084a785
KevinUTN <36267152+KevinUTN@users.noreply.github.com>
2018-07-12 12:10:20 -0300

    Tercera Clase random

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -1,20 +1,21 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var numero
+	var nota;
 
-	numero = parseInt(Math.random()*(11-0)+0);
-	if(numero >= 9){alert("Exelente");}
-	else{
-		if( numero >= 6){alert("Muy bien")}
-	else{
-		if(numero >= 4 ){alert("bien");}
-		else{
-			if(numero >= 2){alert("mal");}
-			else{alert("desaprbado")}
-		}}}}
-		
-	
-	
+	nota = Math.floor(Math.random() * 10) + 1
 
-//FIN DE LA FUNCIÓN
+	if(nota >= 9)
+	{
+		alert("Exelente "+ nota);
+	}
+	else if(nota >3 && nota <9)
+	{
+		alert("Aprobo "+nota);
+	}
+	else
+	{
+		alert("Vamos que la proxima se puede "+nota);
+	}
+
+}//FIN DE LA FUNCIÓN
