39b96cf279dbf7f5d06641f27a9431bdb279d4fe
KevinUTN <36267152+KevinUTN@users.noreply.github.com>
2018-07-13 12:01:35 -0300

    Cuarta Clase Switch

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,24 @@
+function mostrar()
+{
+//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno");
+			break;
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio");
+			break;
+		default:
+			alert("Ya pasamos el frio, ahora el calor");
+			break;
+	}
+}//FIN DE LA FUNCIÓN
